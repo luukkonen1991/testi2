3d45af485386609d3c37b90cde559949ae001dc8
luukkonen1991 <luukkonen1991@gmail.com>
2020-06-09 00:41:56 +0300

    CORRECTED answer

--- testi2.js
+++ testi2.js
@@ -2,6 +2,8 @@ let nr_invites = parseInt(process.argv[2]);
 let depth = parseInt(process.argv[3]);
 
 let users = [];
+let hasInvited = [];
+
 
 if (process.argv[2] && process.argv[3]) {
   let newUser = {
@@ -9,17 +11,21 @@ if (process.argv[2] && process.argv[3]) {
   };
   users.push(newUser);
   console.log(`#${newUser.ID} created`);
-  for (let i = 0; i < depth; i++) {
-    let invitor = users[i].ID;
-    console.log(`#${invitor} is inviting friends`);
-    for (let i = 0; i < nr_invites; i++) {
-      let invitedUser = {
-        ID: i + Math.floor(Math.random() * 1000)
-      };
-      users.push(invitedUser);
-      console.log(`#${invitedUser.ID} created, which was invited by #${invitor}`);
-    }
+  for (let i = 1; i < depth; i++) {
+    users.forEach((invitor) => {
+      if (hasInvited.includes(invitor)) return;
+      console.log(`#${invitor.ID} is inviting friends`);
+      for (let i = 0; i < nr_invites; i++) {
+        let invitedUser = {
+          ID: i + Math.floor(Math.random() * 1000)
+        };
+        users.push(invitedUser);
+        hasInvited.push(invitor);
+        console.log(`#${invitedUser.ID} created, which was invited by #${invitor.ID}`);
+      }
+    });
   }
 } else {
   console.log(`Plese submit the following parameters 'nr_invites' and 'depth'`);
 }
+console.log('All users.amount', users.length);
